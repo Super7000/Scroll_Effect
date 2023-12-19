e78dee8ad368cce428ebda232676df877a720975
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2023-12-19 12:18:11 +0530

    re-structured html layout

--- main.js
+++ main.js
@@ -1,65 +1,55 @@
-let strokeValue = 0;
 let currentTab = 1;
-document.querySelector("body").addEventListener("wheel", (event) => {
-    let prevTab = currentTab;
-    let functions = [];
-    if (event.deltaY > 0 && currentTab < 7) { //Scroll Down condition 
-        strokeValue += 135;
-        currentTab++;
-        document.querySelector(".circle-scroll-bar svg #Dots" + currentTab + " .dotsfill" + currentTab).style.cssText += "fill: rgb(0, 146, 255);";
-        document.querySelector(".circle-scroll-bar svg #Dots" + currentTab + " .dotsstro" + currentTab).style.cssText += "opacity: 1;";
+function main(prevTab) {
+
+    for (var i = 1; i <= currentTab; i++) {
+        document.querySelector(".circle-scroll-bar svg #Dots" + i + " .dotsfill" + i).style.cssText += "fill: rgb(0, 146, 255);";
+        document.querySelector(".circle-scroll-bar svg #Dots" + i + " .dotsstro" + i).style.cssText += "opacity: 1;";
     }
-    else if (event.deltaY < 0 && currentTab > 1) { //Scroll Up condition
-        strokeValue -= 135;
-        document.querySelector(".circle-scroll-bar svg #Dots" + currentTab + " .dotsfill" + currentTab).style.cssText += "fill: rgba(255,255,255,0.5);";
-        document.querySelector(".circle-scroll-bar svg #Dots" + currentTab + " .dotsstro" + currentTab).style.cssText += "opacity: 0;";
-        currentTab--;
+    for (var j = currentTab + 1; j <= 7; j++) {
+        document.querySelector(".circle-scroll-bar svg #Dots" + j + " .dotsfill" + j).style.cssText += "fill: rgba(255,255,255,0.5);";
+        document.querySelector(".circle-scroll-bar svg #Dots" + j + " .dotsstro" + j).style.cssText += "opacity: 0;";
     }
 
     //Changing Progress value of the svg to represent scroll value
+    let strokeValue = 135 * (currentTab - 1);
     document.querySelector(".circle-scroll-bar svg .transrgwht").style.cssText += `stroke-dasharray: ${strokeValue}, 1000;`;
 
 
     if (window.innerWidth <= 768) {
-        if(prevTab < currentTab){
+
+        if (prevTab < currentTab) {
             document.querySelector(".tab.active").style.cssText += "animation: left-to-mid 1s reverse;";
             document.querySelector(".tab" + currentTab).style.cssText += "animation: right-to-mid 1s;";
+        } else if (prevTab > currentTab) {
+            document.querySelector(".tab.active").style.cssText += "animation: right-to-mid 1s reverse;";
+            document.querySelector(".tab" + currentTab).style.cssText += "animation: left-to-mid 1s;";
         }
     }
+
+    // if (window.innerWidth > 768) {
+    //     if (prevTab < currentTab) {
+    //         document.querySelectorAll(".tab.active .main-heading .text-wrapper div").forEach((e) => {
+    //             e.style.cssText += "animation: top-to-mid 0.5s reverse;";
+
+    //         })
+    //         document.querySelectorAll(".tab" + currentTab + " .main-heading .text-wrapper div").forEach((e) => {
+    //             e.style.cssText += "animation: down-to-mid 0.5s;";
+    //         })
+    //     } else {
+    //         document.querySelectorAll(".tab.active .main-heading .text-wrapper div").forEach((e) => {
+    //             e.style.cssText += "animation: down-to-mid 0.5s reverse;";
+    //         })
+    //         document.querySelectorAll(".tab" + currentTab + " .main-heading .text-wrapper div").forEach((e) => {
+    //             e.style.cssText += "animation: top-to-mid 0.5s;";
+    //         })
+    //     }
+    // }
 
 
     //Activating tabs
     document.querySelector(".tab.active").classList.remove("active");
     document.querySelector(".tab" + currentTab).classList.add("active");
 
-
-    //Updating UI based on current tab
-    let subHeadingImg = document.querySelector(".left-container .achivement");
-    let viewCaseCon = document.querySelector(".view-case-container div");
-    let viewCaseConImg = document.querySelector(".view-case-container img");
-    if (currentTab == 7) {
-        viewCaseCon.innerHTML = "Coming Soon";
-        viewCaseCon.style.cssText += "opacity: 0.5;";
-        viewCaseConImg.style.cssText += "display: none;";
-    } else if (currentTab == 1) {
-        subHeadingImg.style.cssText += "display: block;";
-        viewCaseCon.innerHTML = "View Case Study";
-        viewCaseCon.style.cssText += "opacity: 1;";
-    } else {
-        subHeadingImg.style.cssText += "display: none;";
-        viewCaseCon.innerHTML = "View Case Study";
-        viewCaseCon.style.cssText += "opacity: 1;";
-        viewCaseConImg.style.cssText += "display: block;";
-
-    }
-
-    if (currentTab == 3) {
-        document.querySelector(".main-heading img").style.cssText += "display: block;";
-        document.querySelector(".main-heading-text").style.cssText += "display: none;";
-    } else {
-        document.querySelector(".main-heading-text").style.cssText += "display: block;";
-        document.querySelector(".main-heading img").style.cssText += "display: none;";
-    }
 
 
     //Updateing Texts on scroll
@@ -83,14 +73,43 @@ document.querySelector("body").addEventListener("wheel", (event) => {
 
 
     //Changing Background color on scroll
-    let bgColors = ["6411A9", "4E27CE", "0F113B", "17263C", "1457C1", "022A91", "01824D"];
-    document.querySelector(".circle-scroll-bar svg").style.cssText = `background: #${bgColors[tempIndex]};`;
+    let circleSvgBgColors = ["6411A9", "4E27CE", "0F113B", "17263C", "1457C1", "022A91", "01824D"];
+    let bgColors = ["#6411A9", "#4E27CE", "#0F113B", "#17263C", "#124AA3", "#022A91", "#01824D"];
+    document.querySelector(".circle-scroll-bar svg").style.cssText = `background: #${circleSvgBgColors[tempIndex]};`;
+    document.querySelector(".bg").style.cssText = `background: ${bgColors[tempIndex]};`;
+}
+
+let wait = false;
+document.querySelector("body").addEventListener("wheel", (event) => {
+    if (wait == true) return;
+    wait = true;
+    let prevTab = currentTab;
+    if (event.deltaY > 0 && currentTab < 7) { //Scroll Down condition 
+        currentTab++;
+    }
+    else if (event.deltaY < 0 && currentTab > 1) { //Scroll Up condition
+        currentTab--;
+    }
+    main(prevTab)
+    setTimeout(() => {
+        wait = false;
+    }, 500)
+
 })
 
-// ["#6411A9", "6411A9"],
-// ["#4E27CE", "4E27CE"],
-// ["#0F113B", "0F113B"],
-// ["#17263C", "17263C"],
-// ["#124AA3", "1457C1"],
-// ["#022A91", "022A91"],
-// ["#01824D", "01824D"]
+let autoScroll;
+let runing = false;
+window.onresize = () => {
+    if (window.innerWidth <= 768 && runing == false) {
+        runing = true;
+        autoScroll = setInterval(() => {
+            let prevTab = currentTab;
+            currentTab++;
+            if (currentTab > 7) currentTab = 1;
+            main(prevTab);
+        }, 2000)
+    } else if (window.innerWidth > 768 && runing == true) {
+        clearInterval(autoScroll)
+        runing = false;
+    }
+}
