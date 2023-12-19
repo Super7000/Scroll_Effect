3de3e264b8d19a4c9a708279c34143687749dba4
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2023-12-19 21:29:49 +0530

    now its more responsive and swipe event added for mobile

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let currentTab = 1;
-function main(prevTab) {
-
+function main(prevTab = currentTab) {
     for (var i = 1; i <= currentTab; i++) {
         document.querySelector(".circle-scroll-bar svg #Dots" + i + " .dotsfill" + i).style.cssText += "fill: rgb(0, 146, 255);";
         document.querySelector(".circle-scroll-bar svg #Dots" + i + " .dotsstro" + i).style.cssText += "opacity: 1;";
@@ -9,7 +8,6 @@ function main(prevTab) {
         document.querySelector(".circle-scroll-bar svg #Dots" + j + " .dotsfill" + j).style.cssText += "fill: rgba(255,255,255,0.5);";
         document.querySelector(".circle-scroll-bar svg #Dots" + j + " .dotsstro" + j).style.cssText += "opacity: 0;";
     }
-
     //Changing Progress value of the svg to represent scroll value
     let strokeValue = 135 * (currentTab - 1);
     document.querySelector(".circle-scroll-bar svg .transrgwht").style.cssText += `stroke-dasharray: ${strokeValue}, 1000;`;
@@ -93,7 +91,66 @@ function changeUI() {
 
 
 
+//Swipe Function for Mobile
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        let prevTab = currentTab;
+
+        if (xDiff > 0) {
+            // right swipe 
+            if (currentTab < 7)
+                currentTab++;
+        } else {
+            // left swipe 
+            if (currentTab > 1)
+                currentTab--;
+        }
+        main(prevTab);
+        showProgressForMobile();
+
+    } else {
+        if (yDiff > 0) {
+            // down swipe
+        } else {
+            // up swipe 
+        }
+    }
+
+    xDown = null;
+    yDown = null;
+};
+
+
+
 window.onresize = () => {
+    main();
     changeUI();
     autoScrollFunction();
     showProgressForMobile();
@@ -112,7 +169,8 @@ document.querySelector("body").addEventListener("wheel", (event) => {
     else if (event.deltaY < 0 && currentTab > 1) { //Scroll Up condition
         currentTab--;
     }
-    main(prevTab)
+    main(prevTab);
+    showProgressForMobile();
     setTimeout(() => {
         wait = false;
     }, 500)
@@ -138,9 +196,8 @@ changeUI();
 
 
 
-// function updateTextsForMobile(){
 //Updateing Texts on scroll
-
+// function updateTextsForMobile(){
 // let texts = [
 //     ["", "25M+ Downloads", "on appstore & google playstore", "ABC 123", "We are the best web development<br> company in the world"],
 //     ["The Next Big", "Blockchain", "Revolution", "ABC 234", "We are the best web development<br> company in the world"],
